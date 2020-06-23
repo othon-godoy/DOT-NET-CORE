7946ba6f07ab20a131a9b7c72598ad314d84ca73
Othon Godoy <othon.dev@outlook.com>
2020-06-23 08:40:14 -0300

    DOTNET CORE

--- PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,5 +1,7 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Tests
@@ -7,10 +9,56 @@ namespace PaymentContext.Tests
     [TestClass]
     public class StudentTests
     {
-        [TestMethod]
-        public void AdicionarAssinatura()
+        private readonly Name _name;
+        private readonly Document _document;
+        private readonly Email _email;
+        private readonly Address _address;
+        private readonly Student _student;
+
+        public StudentTests()
         {
-            
+            _name = new Name("Bruce", "Wayne");
+            _email = new Email("batman@dc.com");
+            _document = new Document("28695422089", EDocumentType.CPF);
+            _address = new Address("Street 1", "123", "Nice Neighborhood", "Gotham", "NY", "USA", "12345678");
+            _student = new Student(_name, _document, _email);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenHadActiveSubscription()
+        {    
+            var subscription = new Subscription(null);
+
+            var payment = new PayPalPayment("123456789", DateTime.Now, DateTime.Now.AddDays(5), 10, 10, "WAYNE CORP", _document, _address, _email);
+
+            subscription.AddPayment(payment);
+
+            _student.AddSubscription(subscription);
+            _student.AddSubscription(subscription);
+
+            Assert.IsTrue(_student.Invalid);
+        }
+        [TestMethod]
+        public void ShouldReturnErrorWhenSubscriptionHasNoPayment()
+        {            
+            var subscription = new Subscription(null);
+
+            _student.AddSubscription(subscription);
+
+            Assert.IsTrue(_student.Invalid);
+        }
+        [TestMethod]
+        public void ShouldReturnSuccessWhenAddSubscription()
+        {
+            var subscription = new Subscription(null);
+
+            var payment = new PayPalPayment("123456789", DateTime.Now, DateTime.Now.AddDays(5), 10, 10, "WAYNE CORP", _document, _address, _email);
+
+            subscription.AddPayment(payment);
+
+            _student.AddSubscription(subscription);
+
+            Assert.IsTrue(_student.Valid);
         }
     }
 }
---
+++ PaymentContext/PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        // Red, Green, Refactor
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCNPJIsValid()
+        {
+            var doc = new Document("91102779000101", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("79364484037")]
+        [DataRow("58992444028")]
+        [DataRow("80918241065")]
+        public void ShouldReturnSuccessWhenCPFIsValid(string cpf)
+        {
+            var doc = new Document(cpf, EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }        
+    }
+}
